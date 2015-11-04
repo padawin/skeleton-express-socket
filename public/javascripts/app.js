c487ad1fc6af04e34a3a726418c335183672bb21
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-11-04 12:16:20 +0000

    client example to use the socket

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,8 +8,25 @@ function (B) {
 	var host,
 		socket;
 
+	function messageReceived (data) {
+		console.log('Message received');
+		console.log(data);
+	}
+
+	function sendMessage (text) {
+		socket.emit('send-message', { message: text });
+	}
+
+	function confirmationConnectionSocket (data) {
+		console.log(data);
+		console.log('Sent message: Some message');
+		sendMessage('Some message');
+	}
+
 	host = window.location.protocol.concat('//')
 		.concat(window.location.hostname)
 		.concat(':').concat(window.location.port);
-
+	var socket = io.connect(host);
+	socket.on('connection-confirmation', confirmationConnectionSocket);
+	socket.on('message', messageReceived);
 });
